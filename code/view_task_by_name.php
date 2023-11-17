fd66d46c560813bda52cb9ba4b71223f77ef2572
capcrz12 <carlosperezcruz01@gmail.com>
2023-11-17 12:38:34 +0100

    modificaciones api

--- code/view_task_by_name.php
+++ code/view_task_by_name.php
@@ -9,19 +9,34 @@ $con=dbconnection();
 if (isset($_POST["nombre"])) {
     $nombre = $_POST["nombre"];
 
-    $query = "SELECT `idTareas` WHERE `name`=$nombre FROM `tareas`";
+    $query = "SELECT `idTareas` FROM `tareas` WHERE `nombre` = ?";
 
-    $exe=mysqli_query($con,$query);
+    // Preparar la sentencia
+    $stmt = mysqli_prepare($con, $query);
+
+    // Vincular parámetros
+    mysqli_stmt_bind_param($stmt, "s", $nombre);
+
+    // Ejecutar la sentencia
+    $exe = mysqli_stmt_execute($stmt);
 
     if (!$exe) {
+        $arr["success"] = "false";
+        $arr["error"] = "Error en la consulta: " . mysqli_error($con);
         die('Error en la consulta: ' . mysqli_error($con));
     }
 
-    $arr=[];
-    while($row=mysqli_fetch_assoc($exe))
-    {
-        $arr[]=$row;
-    }
+    mysqli_stmt_bind_result($stmt, $idTareas);
+
+    // Obtener el resultado
+    mysqli_stmt_fetch($stmt);
+
+    $arr["idTareas"] = $idTareas;
+    $arr["success"] = "true";
+
+    // Cerrar la sentencia preparada
+    mysqli_stmt_close($stmt);
+    
 } else {
     $arr["success"] = "false";
 }

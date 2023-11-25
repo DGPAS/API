5612909226fb95b1e79da5434fd0f0778c65ed4a
capcrz12 <carlosperezcruz01@gmail.com>
2023-11-25 18:52:44 +0100

    Solucionado delete task borrando tambien pasos asociados

--- code/delete_data.php
+++ code/delete_data.php
@@ -10,27 +10,32 @@ if (isset($_POST["idTareas"])) {
     return;
 }
 
-$query = "DELETE FROM `tareas` WHERE idTareas=?";
-$stmt = mysqli_prepare($con, $query);
+$query1 = "DELETE FROM `pasos` WHERE idTarea=?";
+$query2 = "DELETE FROM `tareas` WHERE idTareas=?";
+$stmt1 = mysqli_prepare($con, $query1);
+$stmt2 = mysqli_prepare($con, $query2);
 
-if (!$stmt) {
+if (!$stmt1 || !$stmt2) {
     echo json_encode(["success" => false, "error" => "Error in prepared statement: " . mysqli_error($con)]);
     return;
 }
 
-mysqli_stmt_bind_param($stmt, "i", $idTareas);
+mysqli_stmt_bind_param($stmt1, "i", $idTareas);
+$exe1 = mysqli_stmt_execute($stmt1);
 
-$exe = mysqli_stmt_execute($stmt);
+mysqli_stmt_bind_param($stmt2, "i", $idTareas);
+$exe2 = mysqli_stmt_execute($stmt2);
 
 $arr = [];
-if ($exe) {
+if ($exe1 && $exe2) {
     $arr["success"] = true;
 } else {
     $arr["success"] = false;
     $arr["error"] = mysqli_error($con);
 }
 
-mysqli_stmt_close($stmt);
+mysqli_stmt_close($stmt1);
+mysqli_stmt_close($stmt2);
 
 echo json_encode($arr);
 

a2fab76e521b8878163f05a78a98828fdb2eea89
capcrz12 <carlosperezcruz01@gmail.com>
2023-11-28 22:36:22 +0100

    Añadido modificacion de password alumnos

--- code/upload_password.php
+++ code/upload_password.php
@@ -2,22 +2,30 @@
 
 include("dbconnection.php");
 $con = dbconnection();
-$home = getenv('HOME');
 
-    if (isset($_FILES['image']) && isset($_POST['idStudent'])) {
-        $image = $_FILES['image']['name'];
+    if (isset($_FILES['pictograma1']) && isset($_FILES['pictograma2']) && isset($_FILES['pictograma3']) && isset($_POST['idStudent'])) {
+        $pictograma1 = $_FILES['pictograma1']['name'];
+        $pictograma2 = $_FILES['pictograma2']['name'];
+        $pictograma3 = $_FILES['pictograma3']['name'];
         $idStudent = $_POST['idStudent'];
 
-        $imagePath = 'images/students/passwords/' . $image;
-        $tmp_name = $_FILES['image']['tmp_name'];
+        $pictograma1Path = 'images/students/passwords/' . $pictograma1;
+        $tmp_name1 = $_FILES['pictograma1']['tmp_name'];
+        move_uploaded_file($tmp_name1, $pictograma1Path);
 
-        move_uploaded_file($tmp_name, $imagePath);
+        $pictograma2Path = 'images/students/passwords/' . $pictograma2;
+        $tmp_name2 = $_FILES['pictograma2']['tmp_name'];
+        move_uploaded_file($tmp_name2, $pictograma2Path);
 
-        $query = "UPDATE `Alumno` SET `password`=? WHERE `id`=?";
+        $pictograma3Path = 'images/students/passwords/' . $pictograma3;
+        $tmp_name3 = $_FILES['pictograma3']['tmp_name'];
+        move_uploaded_file($tmp_name3, $pictograma3Path);
+
+        $query = "INSERT INTO `passwordAlumno`(`idAlumno`, `pictograma1`, `pictograma2`, `pictograma3`) VALUES (?, ?, ?, ?)";
 
         $stmt = mysqli_prepare($con, $query);
-        
-        mysqli_stmt_bind_param($stmt, "si", $image, $idStudent);
+
+        mysqli_stmt_bind_param($stmt, "isss", $idStudent, $pictograma1, $pictograma2, $pictograma3);
 
         $exe = mysqli_stmt_execute($stmt);
 

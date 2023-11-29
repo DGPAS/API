4f2fc50f85e451a859ffb2fd6b936d7b97ab6c5b
capcrz12 <carlosperezcruz01@gmail.com>
2023-11-29 17:02:29 +0100

    Añadido manejo de video

---
+++ code/saveVideo.php
@@ -0,0 +1,42 @@
+<?php
+
+include("dbconnection.php");
+$con = dbconnection();
+
+$home = getenv('HOME');
+
+if (isset($_FILES['video']) && isset($_POST['idTareas'])) {
+    $video = $_FILES['video']['name'];
+    $idTareas = $_POST['idTareas'];
+
+    $videoPath = 'video/' . $video;
+    $tmp_name = $_FILES['video']['tmp_name'];
+
+    move_uploaded_file($tmp_name, $videoPath);
+
+    $query = "UPDATE `tareas` SET `video`=? WHERE `idTareas`=?";
+
+    $stmt = mysqli_prepare($con, $query);
+
+    mysqli_stmt_bind_param($stmt, "si", $video, $idTareas);
+
+    $exe = mysqli_stmt_execute($stmt);
+
+    $arr = [];
+
+    if ($exe) {
+        $arr["success"] = "true";
+        $arr["idTareas"] = $idTareas; // Use the provided idTareas directly
+    } else {
+        $arr["success"] = "false";
+        $arr["error"] = mysqli_error($con);
+    }
+
+    mysqli_stmt_close($stmt);
+
+    echo json_encode($arr);
+} else {
+    $arr["success"] = "false";
+    die('Error in parameter passing: ' . mysqli_error());
+}
+?>

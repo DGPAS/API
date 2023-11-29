ec4d84fb62edb5000aee98a573859624898476b8
capcrz12 <carlosperezcruz01@gmail.com>
2023-11-30 00:09:38 +0100

    Ficheros para obtener las tareas asociadas a un alumno

---
+++ code/view_student_tasks.php
@@ -0,0 +1,48 @@
+
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+include("dbconnection.php");
+$con=dbconnection();
+
+
+if (isset($_GET["idTarea"])) {
+    $idTarea = $_GET["idTarea"];
+
+    $query = "SELECT `nombre`, `descripcion`, `miniatura`, `video` FROM `tareas` WHERE `idTareas` = ?";
+
+    // Preparar la sentencia
+    $stmt = mysqli_prepare($con, $query);
+
+    // Vincular parámetros
+    mysqli_stmt_bind_param($stmt, "i", $idTarea);
+
+    // Ejecutar la sentencia
+    $exe = mysqli_stmt_execute($stmt);
+
+    if (!$exe) {
+        $arr["success"] = "false";
+        $arr["error"] = "Error en la consulta: " . mysqli_error($con);
+        die('Error en la consulta: ' . mysqli_error($con));
+    }
+
+    $arr["success"] = "true";
+
+    $result = mysqli_stmt_get_result($stmt);
+
+
+    while($row=mysqli_fetch_assoc($result))
+    {
+        $arr[]=$row;
+    }
+
+    // Cerrar la sentencia preparada
+    mysqli_stmt_close($stmt);
+    
+} else {
+    $arr["success"] = "false";
+}
+
+print(json_encode($arr));
+?>

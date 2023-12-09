28c7bbfd54f5164000b626258ce1c3f6c4d5475d
capcrz12 <carlosperezcruz01@gmail.com>
2023-12-09 12:19:20 +0100

    Cambiados nombres que estaban mal por inconsistencias

--- code/saveVideo.php
+++ code/saveVideo.php
@@ -5,20 +5,20 @@ $con = dbconnection();
 
 $home = getenv('HOME');
 
-if (isset($_FILES['video']) && isset($_POST['idTasks'])) {
+if (isset($_FILES['video']) && isset($_POST['idTask'])) {
     $video = $_FILES['video']['name'];
-    $idTasks = $_POST['idTasks'];
+    $idTask = $_POST['idTask'];
 
     $videoPath = 'video/' . $video;
     $tmp_name = $_FILES['video']['tmp_name'];
 
     move_uploaded_file($tmp_name, $videoPath);
 
-    $query = "UPDATE `tasks` SET `video`=? WHERE `idTasks`=?";
+    $query = "UPDATE `tasks` SET `video`=? WHERE `idTask`=?";
 
     $stmt = mysqli_prepare($con, $query);
 
-    mysqli_stmt_bind_param($stmt, "si", $video, $idTasks);
+    mysqli_stmt_bind_param($stmt, "si", $video, $idTask);
 
     $exe = mysqli_stmt_execute($stmt);
 
@@ -26,7 +26,7 @@ if (isset($_FILES['video']) && isset($_POST['idTasks'])) {
 
     if ($exe) {
         $arr["success"] = "true";
-        $arr["idTasks"] = $idTasks; // Use the provided idTasks directly
+        $arr["idTask"] = $idTask; // Use the provided idTask directly
     } else {
         $arr["success"] = "false";
         $arr["error"] = mysqli_error($con);
--- code/update_data.php
+++ code/update_data.php
@@ -3,38 +3,38 @@
 include("dbconnection.php");
 $con = dbconnection();
 
-if (isset($_POST["idTasks"])) {
+if (isset($_POST["idTask"])) {
 
-    $idTasks = $_POST["idTasks"];
+    $idTask = $_POST["idTask"];
 
     if (isset($_POST["taskName"]) && isset($_POST["description"])) {
         $taskName = $_POST["taskName"];
         $description = $_POST["description"];
 
-        $query = "UPDATE `tasks` SET `taskName`=?, `description`=? WHERE `idTasks`=?";
+        $query = "UPDATE `tasks` SET `taskName`=?, `description`=? WHERE `idTask`=?";
 
         $stmt = mysqli_prepare($con, $query);
         
-        mysqli_stmt_bind_param($stmt, "ssi", $taskName, $description, $idTasks);
+        mysqli_stmt_bind_param($stmt, "ssi", $taskName, $description, $idTask);
     }
     else if (isset($_POST["taskName"])) {
         $taskName = $_POST["taskName"];
 
-        $query = "UPDATE `tareas` SET `taskName`=? WHERE `idTasks`=?";
+        $query = "UPDATE `tareas` SET `taskName`=? WHERE `idTask`=?";
 
         $stmt = mysqli_prepare($con, $query);
         
-        mysqli_stmt_bind_param($stmt, "si", $taskName, $idTasks);
+        mysqli_stmt_bind_param($stmt, "si", $taskName, $idTask);
     }
     
     else {
         $description = $_POST["description"];
 
-        $query = "UPDATE `tasks` SET `description`=? WHERE `idTasks`=?";
+        $query = "UPDATE `tasks` SET `description`=? WHERE `idTask`=?";
 
         $stmt = mysqli_prepare($con, $query);
         
-        mysqli_stmt_bind_param($stmt, "si", $description, $idTasks);
+        mysqli_stmt_bind_param($stmt, "si", $description, $idTask);
     }
 
     if (isset($_POST["taskName"]) || isset($_POST["description"])) {
